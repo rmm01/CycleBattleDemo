cfa0e342ac34fd38f10d238799719ba801b8c567
Ricky Martinez <crosscountry68@gmail.com>
2015-12-11 20:39:24 -0800

    Removed toolbar from home activity. added text view for app title.

--- app/src/main/java/com/yckir/cyclebattledemo/HomeActivity.java
+++ app/src/main/java/com/yckir/cyclebattledemo/HomeActivity.java
@@ -7,7 +7,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
@@ -16,8 +15,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
 
