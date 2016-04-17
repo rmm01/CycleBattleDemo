d795dbe7469f4912b244aa6ba88138545d9165c0
Ricky Martinez <crosscountry68@gmail.com>
2016-04-17 14:16:24 -0700

    AlarmHandler      - removed unnecessary methods.

--- app/src/main/java/com/yckir/cyclebattledemo/utility/AlarmHandler.java
+++ app/src/main/java/com/yckir/cyclebattledemo/utility/AlarmHandler.java
@@ -9,7 +9,6 @@ import android.os.Message;
  */
 public class AlarmHandler extends Handler {
     public static final String TAG="ALARM_HANDLER";
-    private boolean started;
     private AlarmListener mTimerListener;
 
 
@@ -18,7 +17,6 @@ public class AlarmHandler extends Handler {
      * @param listener the listener that will be notified when the wait time is over
      */
     public AlarmHandler(AlarmListener listener){
-        started=false;
         mTimerListener=listener;
     }
 
@@ -30,45 +28,26 @@ public class AlarmHandler extends Handler {
      */
     @Override
     public void handleMessage(Message msg) {
-        if(started && mTimerListener != null)
+        if(mTimerListener != null)
             mTimerListener.alarm( msg.what );
     }
 
 
     /**
-     * Turn on the alarm and sets how long to wait until the AlarmListener is notified.
-     * The Alarm is considered to be on until turnOffAlarm() is called.
+     * Sets how long to wait until the AlarmListener is notified.
      *
      * @param delay how long to wait in milliseconds until the AlarmListener is notified
      * @param id the id for the for the alarm
      */
     public  void setAlarm(long delay, int id){
-        started=true;
         sendEmptyMessageDelayed(id, delay);
-    }
-
-
-    /**
-     * Turns off the alarm. AlarmListener will not be been notified if it hasn't already.
-     */
-    public void turnOffAlarm(){
-        started=false;
-    }
-
-
-    /**
-     * returns whether the alarm is on
-     * @return true if the alarm is on, false otherwise
-     */
-    public boolean isAlarmOn(){
-        return started;
     }
 
 
     /**
      * Listener used ny AlarmHandler to notify that a waiting period is over
      */
-    interface AlarmListener {
+    public interface AlarmListener {
         /**
          * Will be called by AlarmHandler when a waiting period is over
          *
