75c44b22339304997776ea7db83df35439bc1715
Ricky Martinez <crosscountry68@gmail.com>
2016-04-21 13:03:42 -0700

    Settings activity no longer shows title bar and changed theme from light to dark.

--- app/src/main/java/com/yckir/cyclebattledemo/activities/SettingsActivity.java
+++ app/src/main/java/com/yckir/cyclebattledemo/activities/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.yckir.cyclebattledemo.activities;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.yckir.cyclebattledemo.R;
 
@@ -9,7 +11,16 @@ public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        disableStatusBar();
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    /**
+     * Disables the status bar
+     */
+    private void disableStatusBar(){
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
